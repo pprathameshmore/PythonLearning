640980e2b543039578d9675741b0a52e024f7281
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-19 00:34:54 +0530

    IfElse in Python

---
+++ IfElse.py
@@ -0,0 +1,16 @@
+#@Author Prathamesh More
+
+a = 20
+b = 10
+
+if (a == b):
+    print('Both are equals')
+
+elif (a > b):
+    print('a is greater than b')
+
+elif (a < b):
+    print('b is greater than a')
+
+elif (a != b):
+    print('Both are not equal')
