15e5888e81971753b3d49861eb09e4327b515583
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-20 06:03:35 +0530

    ComparisonOperators

---
+++ ComparisonOperators.py
@@ -0,0 +1,16 @@
+#@Athor Prathamesh More
+
+a = input('Value of a : ')
+b = input('Value of b : ')
+
+if a == b :
+    print('Both are same')
+
+elif a > b:
+    print("a is greater than b")
+
+elif b > a:
+    print("b is greater than a")
+
+elif a != b:
+    print('a is not equal to b')
