e93f54bd826cfefd01816af493b62634f7e4710f
Prathamesh More <moreprathameshmore@hotmail.com>
2018-02-02 17:37:53 +0530

    Added more file related operations

---
+++ FileOperations_2.py
@@ -0,0 +1,12 @@
+#@Author Prathamesh More
+
+import os
+
+#os.rename("OriginalFile.txt","NewFile.txt")
+
+#os.remove("RemoveFile.txt")
+os.mkdir("New Directory")
+
+os.getcwd()
+
+os.rmdir("New Directory")
