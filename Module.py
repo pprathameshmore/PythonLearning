c037ee446d984598e71682f1757ff72387545d98
Prathamesh More <moreprathameshmore@hotmail.com>
2018-02-02 16:50:11 +0530

    Added Module Example
    A module allows you to logically organize your Python code. Grouping related code into a
    module makes the code easier to understand and use. A module is a Python object with
    arbitrarily named attributes that you can bind and reference.
    Simply, a module is a file consisting of Python code. A module can define functions, classes
    and variables. A module can also include runnable code.

---
+++ Module.py
@@ -0,0 +1,14 @@
+#@Author Prathamesh More
+
+"""
+A module allows you to logically organize your Python code. Grouping related code into a
+module makes the code easier to understand and use. A module is a Python object with
+arbitrarily named attributes that you can bind and reference.
+Simply, a module is a file consisting of Python code. A module can define functions, classes
+and variables. A module can also include runnable code.
+"""
+
+import support
+
+support.sayHelloToZara("Prathamesh")
+support.sayHelloToZara("Zara")
---
+++ support.py
@@ -0,0 +1,8 @@
+#@Author Prathamesh More
+
+#Sample Module File
+
+def sayHelloToZara(name):
+
+    print("Hello ",name)
+
